3c8b931ee47a77ff163a5eb2314b5b0495e75601
LFGstudio <38088103+LFGstudio@users.noreply.github.com>
2023-09-24 02:17:49 +0200

    Update not-found.js

--- src/views/not-found.js
+++ src/views/not-found.js
@@ -8,15 +8,15 @@ const NotFound = (props) => {
   return (
     <div className="not-found-container">
       <Helmet>
-        <title>404 - Not Found</title>
+        <title>404 </title>
       </Helmet>
-      <h3>OOPS! PAGE NOT FOUND</h3>
+      <h3></h3>
       <div className="not-found-container1">
         <h1 className="not-found-text1">404</h1>
       </div>
       <div className="not-found-container2">
         <h2 className="not-found-text2">
-          WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
+        
         </h2>
       </div>
     </div>
